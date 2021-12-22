07c6717722755a8b2a616566754ecea29e1a7adf
Cesar Level <cesarlevel@gmail.com>
2021-12-22 16:55:59 -0500

    Complete

--- library.js
+++ library.js
@@ -1,25 +1,37 @@
+const formContiner = document.querySelector('#add-book-form');
+const addBookForm = document.querySelector('form');
+const cancelForm = document.querySelector('form button.is-secondary');
+const formErrorMessage = document.querySelector('form p.error-message');
+
 class Book {
-    constructor(title, author, numberOfPages, read = false) {
+    constructor(title, author, numberOfPages = 2, read = false, background) {
         this.title = title;
         this.author = author;
         this.numberOfPages = numberOfPages;
         this.read = read;
+        this.background = background || this.selectRandomColor();
+    }
+
+    static colors = ['aliceblue', 'antiquewhite', 'azure', 'blanchedalmond', 'cornsilk', 'gainsboro', 'lavender', 'thistle', 'salmon'];
+
+    get isRead() {
+        return this.read ? 'Read' : 'Not read';
     }
 
     changeReadStatus() {
         this.read = !this.read;
     }
 
-    get isRead() {
-        return this.read;
+    selectRandomColor() {
+        return Book.colors[Math.floor(Math.random() * Book.colors.length)];
     }
 }
 
-const formContiner = document.querySelector('#add-book-form');
-const addBookForm =  document.querySelector('form');
-
-let library = [new Book('hobbit wqeqwe  qweqweqweqwe', 'HR Tolkien', '234', true), new Book('hobbit', 'HR Tolkien', '234', true), new Book('hobbit', 'HR Tolkien', '234', true), new Book('hobbit', 'HR Tolkien', '234', true), new Book('hobbit', 'HR Tolkien', '234', true), new Book('hobbit', 'HR Tolkien', '234', true)];
-
+let library = [
+    new Book('In search of lost time', 'Marcel Proust', '432', true),
+    new Book('Ulysses', 'James Joyce', '145', true),
+    new Book('Don Quixote', 'Miguel de Cervantes', '589', true),
+];
 
 addBookForm.onsubmit = function(e) {
     e.preventDefault();
@@ -28,27 +40,38 @@ addBookForm.onsubmit = function(e) {
     const {value: numberOfPagesValue} = e.target.querySelector('#number-of-pages');
     const {checked: readValue} = e.target.querySelector('#read');
     const newBook = new Book(titleValue, authorValue, numberOfPagesValue, readValue);
-    if (library.some(({title}) => title === titleValue)) {
-        console.log('gotcha')
+    if (library.some(({title}) => title.toLowerCase() === titleValue.toLowerCase())) {
+        formErrorMessage.innerHTML = 'Book is already in your library';
     } else {
         library.push(newBook);
-        renderCollection(library.collection);
+        e.target.reset();
+        hideForm();
+        setLocalStorage();
+        renderCollection(library);
     }
 }
 
-function renderCollection(collection) {
+cancelForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideForm();
+})
+
+function renderCollection(library) {
     const shelf = document.querySelector('#library #shelf');
     shelf.innerHTML = '';
     library.forEach((book, i) => {
         shelf.insertAdjacentHTML('beforeend', `
-            <div class="book" title="${book.title}">
-                ${book.title}, ${book.isRead}
-                <small>${book.author}</small>
-                <div onclick="test(${i})">Delete</div>
+            <div class="book" title="${book.title}" style="background: ${book.background}">
+                <div class="title">
+                    <span>${book.title}</span>
+                    <small>${book.author}</br><i>P. ${book.numberOfPages}</i></small>
+                </div>
+                <div class="mark-as-read ${book.read ? 'is-read' : 'is-not-read'}" onclick="markBookAsRead(${i})">${book.isRead}</div>
+                <div class="delete-book-button" onclick="deleteBook(${i})"><i class="uil uil-times-circle"></i></div>
             </div>
         `);
     });
-    shelf.insertAdjacentHTML('beforeend', `<div class="book is-new" onclick="showForm()">+</div>`)
+    shelf.insertAdjacentHTML('beforeend', `<div class="book is-new" onclick="showForm()"><i class="uil uil-plus-circle"></i></div>`);
 }
 
 function showForm() {
@@ -56,14 +79,41 @@ function showForm() {
     document.body.classList.add('no-scroll');
 }
 
+function hideForm() {
+    formContiner.classList.remove('is-visible');
+    document.body.classList.remove('no-scroll');
+}
+
 function deleteBook(bookIndex) {
     library.splice(bookIndex, 1);
+    setLocalStorage();
+    renderCollection(library);
 }
 
-function test(index) {
+function markBookAsRead(index) {
     library[index].changeReadStatus(index);
-    renderCollection(library.collection);
+    setLocalStorage();
+    renderCollection(library);
 }
 
-renderCollection(library)
-console.log(library)
+function setLocalStorage() {
+    window.localStorage.setItem('odin-library', JSON.stringify(library));
+}
+
+function getLocalStorage() {
+    let localLibrary = JSON.parse(window.localStorage.getItem('odin-library'));
+    library = localLibrary.map(({title, author, numberOfPages, read, background}) => {
+        return new Book(title, author, numberOfPages, read, background);
+    })
+}
+
+function init() {
+    if (window.localStorage.getItem('odin-library')) {
+        getLocalStorage();
+    } else {
+        setLocalStorage();
+    }
+    renderCollection(library);
+}
+
+init();
